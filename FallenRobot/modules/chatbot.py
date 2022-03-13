a46503df2716c39add5ecfefa267f6340e036c68
Mr Lucky <94621036+Mrvk1703@users.noreply.github.com>
2022-03-13 20:48:47 +0530

    Update chatbot.py

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -40,7 +40,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Fallen Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
+                "Lucky Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -66,7 +66,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Fallen Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
+                "Lucky Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
